8e3b00f147700b5cf11b8942aac421c52ef8b26b
Miguelr128 <miguelr128@gmail.com>
2018-04-08 18:13:49 -0600

    Primera version del sort

--- sort.py
+++ sort.py
@@ -1,3 +1,10 @@
 def sortList(A):
     """Método que ordena una lista."""
+    for i in range(1, len(A)):
+        b = A[i]
+        j = i -1
+        while j >= 0 and b < A[j]:
+            A[j+1] = A[j]
+            j -= 1
+        A[j+1] = b   
     return A
