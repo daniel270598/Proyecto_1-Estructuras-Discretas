c0a355bfa6ca8e08c986cc96b93b2629ae91067a
daniel270598 <daniel270598@gmail.com>
2018-04-11 15:51:21 -0600

    Arreglo del problema en el indice.

--- Medians.py
+++ Medians.py
@@ -5,11 +5,11 @@ def sortList(A):
 def Median_Of_Median(A,i):
     """Método que busca la mediana de la lista."""
     sublistas = [A[j:j+5] for j in range(0,len(A),5)]
-    medianas = [sortList(sublistas[i])[len(sublistas[i]/2)] for i in range(0,len(sublistas))]
+    medianas = [sortList(sublistas[i])[len(sublistas[i]//2)] for i in range(0,len(sublistas))]
     
     #-----------------Aquí se evalúa el caso base de las medianas---------------------------
     if (len(medianas) <= 5):
-        pivot = sortList(medianas)[len(medianas)/2]
+        pivot = sortList(medianas)[len(medianas)//2]
     else:
         pivot = Median_Of_Median(medianas,0)
 
