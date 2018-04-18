61a8fc85bdd86b7cb4c58a6a9f53edd8eea47a27
Miguelr128 <miguelr128@gmail.com>
2018-04-18 14:40:49 -0600

    Arreglo de nuevo

--- Medians.py
+++ Medians.py
@@ -11,16 +11,6 @@ def median_of_medians(A,i):
         pivot = sort.sort5(medianas)[len(medianas)//2]
     else:
         pivot = median_of_medians(medianas,len(medianas)//2)
-=======
-    sublistas = [A[j:j+5] for j in range(0,len(A),5)]
-    medianas = [sort5(sublistas[i])[len(sublistas//2)] for i in range(0,len(sublistas))]
-    
-    #-----------------Aquí se evalúa el caso base de las medianas---------------------------
-    if (len(medianas) <= 5):
-        pivot = sort5(medianas)[len(medianas)//2]
-    else:
-        pivot = median_of_medians(medianas,0)
->>>>>>> 3e94aa814a39643a6d2d4adf4377ae7b58ffb308
 
     #------------ Se realiza la particion del pivot para obtener la mediana de las medianas-------------
     l = [j for j in A if j < pivot]
