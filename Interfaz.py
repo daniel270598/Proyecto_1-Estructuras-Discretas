6ccb824c4d4179b5c0413b607313bc99b57ae710
daniel270598 <daniel270598@gmail.com>
2018-04-18 17:56:39 -0600

    Modificacion Clase Interfaz

--- Interfaz.py
+++ Interfaz.py
@@ -31,8 +31,28 @@ class Interfaz:
 
     # Método el cual imprimirá el sub-menú para el Quicksort.
     def menu_Quicksort(self):
+        """
+            * Método menu_Quicksort:
+            * Método el cual desarrollara un bloque de impresión
+            * con las diferentes opciones para el usuario en base a el Quicksort.
+        """
+        print("\n--- Bienvenido al Menu Quicksort ---")
+        print("\n\n1. Modificar el arreglo A.")
+        print("\n2. Quicksort.")
+        print("\n3. Median of Medians.")
+        print("\n4. Salir.")
         pass
 
     # Método el cual imprimirá el sub-menú para Median of Medians.
     def menu_Median_of_Medians(self):
+        """
+            * Método menu_Median_of_Medians:
+            * Método el cual desarrollara un bloque de impresión
+            * con las diferentes opciones para el usuario en base a el Median_of_Medians.
+        """
+        print("\n--- Bienvenido al Menu Median_of_Medians ---")
+        print("\n\n1. Modificar el arreglo A.")
+        print("\n2. Quicksort.")
+        print("\n3. Median of Medians.")
+        print("\n4. Salir.")
         pass
