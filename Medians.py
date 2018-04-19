545eac6d6ed2d4bd2e089eaedfdf53b3d02c7b13
daniel270598 <daniel270598@gmail.com>
2018-04-19 09:10:08 -0600

    Creación Clase Medianas

--- Medians.py
+++ Medians.py
@@ -1,24 +1,31 @@
-from sort import sort5
+from sort import sort5                                              # Importo sort5 del módulo sort.
+import cProfile                                                     # Importo cProfile para realizar profilling.
 
-def median_of_medians(A,i):
-    """Método que busca la mediana de la lista."""
-    sublistas = [A[j:j+5] for j in range(0,len(A),5)]
-    medianas = [sort5(sublista)[len(sublista)//2] for sublista in sublistas]
-    
-    #-----------------Aquí se evalúa el caso base de las medianas---------------------------
-    if (len(medianas) <= 5):
-        pivot = sort5(medianas)[len(medianas)//2]
-    else:
-        pivot = median_of_medians(medianas,len(medianas)//2)
+# __________________________________________________________________ Clase Medianas.
+class Medianas:
 
-    #------------ Se realiza la particion del pivot para obtener la mediana de las medianas-------------
-    l = [j for j in A if j < pivot]
-    h = [j for j in A if j > pivot]
+    def median_of_medians(A,i):
+        """Método que busca la mediana de la lista."""
+        sublistas = [A[j:j+5] for j in range(0,len(A),5)]
+        medianas = [sort5(sublista)[len(sublista)//2] for sublista in sublistas]
+        
+        #-----------------Aquí se evalúa el caso base de las medianas---------------------------
+        if (len(medianas) <= 5):
+            pivot = sort5(medianas)[len(medianas)//2]
+        else:
+            pivot = median_of_medians(medianas,len(medianas)//2)
 
-    k = len(l)
-    if i < k:
-        return median_of_medians(l,i)
-    elif i > k:
-        return median_of_medians(h,i-k-1)
-    else: #pivot = k
-        return pivot
+        #------------ Se realiza la particion del pivot para obtener la mediana de las medianas-------------
+        l = [j for j in A if j < pivot]
+        h = [j for j in A if j > pivot]
+
+        k = len(l)
+        if i < k:
+            return median_of_medians(l,i)
+        elif i > k:
+            return median_of_medians(h,i-k-1)
+        else: #pivot = k
+            return pivot
+
+    def timing_Median_of_Medians():
+        cProfile.run(median_of_medians())
