d81106adcfcf5a6434910cd64f20b35fe6431ad5
daniel270598 <daniel270598@gmail.com>
2018-04-18 21:55:58 -0600

    Cambios Funcionamiento Medianas

--- Control.py
+++ Control.py
@@ -1,9 +1,13 @@
 """
-    * Clase Control:
+    * Módulo principal aquí se localiza el método main.
+    * Clase Control: 
 """
 
-from Interfaz import Interfaz                                       # Importa la clase Interfaz
+from Interfaz import Interfaz                                       # Importa la clase Interfaz.
+import QuickSort as qs                                              # Importa el Quicksort.
+import Medians as ms                                                # Importa el módulo Medianas.
 import os                                                           # Importa para accesar al sistema.
+import msvcrt                                                       # Importa.
 
 # _________________________________________________________________ Define la clase Control
 class Control:
@@ -18,8 +22,7 @@ class Control:
     # ------------------------------------------------------------- Método el cual se encargará de llamar a los 
     # ------------------------------------------------------------- diferentes métodos para continuar el flujo
     # ------------------------------------------------------------- del programa.
-    @staticmethod
-    def secuencia():
+    def secuencia(self):
         """
             * Método secuencia:
             * Método el cual se encarga de llamar a los 
@@ -27,27 +30,72 @@ class Control:
             * flujo del programa.
         """
         opc = 0
-        while (opc != 4):
-            os.system('cls')                                        # Limpia pantalla
+        while (opc != 3):
+            os.system('cls')                                        # Limpia pantalla.
             cInterfaz = Interfaz()                                  # Creo el objeto Interfaz.
             cInterfaz.menu_Principal()                              # Llamamos al método externo.
-            opc = int(input("\n\nIngrese la opcion: "))             # Aquí almacenamos en opc lo que digite el usuario.
+            opc = int(input("\nIngrese la opcion: "))               # Aquí almacenamos en opc lo que digite el usuario.
             # ______________________________________________________ Bloque condicional el cual evalúa lo que
             #                                                        ingreso el usuario.
-            if opc == 1:                                            # Modificar Lista
-                os.system('cls')                                    # Limpia pantalla
-            elif opc == 2:                                          # Submenu Quicksort.
-                os.system('cls')                                    # Limpia pantalla
-                cInterfaz.menu_Quicksort()                          # Llama método externo.
-            elif opc == 3:                                          # Submenu Medianas.
-                os.system('cls')                                    # Limpia pantalla
-                cInterfaz.menu_Median_of_Medians()                  # Llama método externo.
-            elif opc == 4:                                          # Salir.
-                pass#exit()                                         # Sale del programa.
+            if opc == 1:                                            # Submenu Quicksort.
+                self.control_menu_Quicksort()                       # Llama al método interno.
+            elif opc == 2:                                          # Submenu Medianas.
+                self.control_menu_Medians()                         # Llama al método interno.
+            elif opc == 3:                                          # Salir.
+                exit()                                              # Sale del programa.
             # ______________________________________________________ Fin del bloque.
+        pass
+
+    # -------------------------------------------------------------- Método que permite crea una lista
+    def crearLista(self):
+        a = []                                                      # Creamos una lista vacía.
+        # __________________________________________________________ Ciclo para agregar elementos.
+        while (len(a) == 0) or ((msvcrt.getch().decode()) != chr(27)):  # Guarda la tecla presionada por el usuario.
+            os.system("cls")                                            # Limpia pantalla.
+            print("--- Agregando Elementos a la Lista ---")             # Mensaje al usuario.
+            print("\na = {}".format(a))
+            # ------------------------------------------------------ Aquí almacenamos en la lista lo que digite el usuario.
+            a.append(int(input("\nIngrese el elemento: ")))
+            print("Presiones ESC para salir, otra tecla para continuar..")
+        return a                                                    # Retornamos la lista.
+
+    # -------------------------------------------------------------- Método el cual permite manejar el menu - quicksort.
+    def control_menu_Quicksort(self):
+        opc2 = 0                                                    # Inicializa opc2.
+        cInterfaz = Interfaz()                                      # Creo el objeto Interfaz.
+        #___________________________________________________________ Inicio del ciclo while - Iterar menú.
+        while (opc2 != 4): # ///////////// Numero a poner el de la opcion SALIR ///////////////////////
+            os.system('cls')                                            # Limpia pantalla
+            cInterfaz.menu_Quicksort()                              # Llama método externo.
+            opc2 = int(input("\nIngrese la opcion: "))              # Aquí almacenamos en opc2 lo que digite el usuario.
+            #------------------------------------------------------- Bloque de evaluación para menu quicksort.
+            if opc2 == 1:
+                os.system("cls")                                    # Limpia pantalla
+            #qs.quickSort()
+            #------------------------------------------------------- Fin bloque de evaluación.
+            #_______________________________________________________ Fin bloque while.
+        pass
+
+    # -------------------------------------------------------------- Método el cual permite manejar el menu - Medians.
+    def control_menu_Medians(self):
+        opc2 = 0                                                    # Inicializa opc2.
+        cInterfaz = Interfaz()                                      # Creo el objeto Interfaz.
+        #___________________________________________________________ Inicio del ciclo while - Iterar menú.
+        while (opc2 != 7):
+            os.system('cls')                                        # Limpia pantalla.
+            cInterfaz.menu_Median_of_Medians()                      # Llama método externo.
+            opc2 = int(input("\nIngrese la opcion: "))              # Aquí almacenamos en opc2 lo que digite el usuario.
+            #------------------------------------------------------- Bloque de evaluación para menu quicksort.
+            if opc2 == 1:
+                self.crearLista()
+            elif opc2 == 2:
+                os.system("cls")                    
+            #------------------------------------------------------- Fin bloque de evaluación.
+        #___________________________________________________________ Fin bloque while.
         pass
 
 # ------------------------------------------------------------------ Bloque Principal - Main
 if __name__ == "__main__":
-    Control.secuencia()
+    contr = Control()
+    contr.secuencia()
     pass
--- Interfaz.py
+++ Interfaz.py
@@ -18,10 +18,9 @@ class Interfaz:
             * con las diferentes opciones para el usuario.
         """
         print("\n--- Bienvenido al Menu Principal ---")
-        print("\n1. Modificar el arreglo A.")
-        print("2. Quicksort.")
-        print("3. Median of Medians.")
-        print("\n4. Salir.")
+        print("\n1. Quicksort.")
+        print("2. Median of Medians.")
+        print("\n3. Salir.")
         pass
 
     # Método el cual imprimirá el sub-menú para el Quicksort.
@@ -32,9 +31,9 @@ class Interfaz:
             * con las diferentes opciones para el usuario en base a el Quicksort.
         """
         print("\n--- Bienvenido al Menu Quicksort ---")
-        print("\n\n1. Modificar el arreglo A.")
-        print("\n2. Quicksort.")
-        print("\n3. Median of Medians.")
+        print("\n1. Modificar el arreglo A.")
+        print("2. Quicksort.")
+        print("3. Median of Medians.")
         print("\n4. Salir.")
         pass
 
@@ -46,8 +45,11 @@ class Interfaz:
             * con las diferentes opciones para el usuario en base a el Median_of_Medians.
         """
         print("\n--- Bienvenido al Menu Median_of_Medians ---")
-        print("\n\n1. Modificar el arreglo A.")
-        print("\n2. Quicksort.")
-        print("\n3. Median of Medians.")
-        print("\n4. Salir.")
+        print("\n1. Crear Lista.")
+        print("2. Prueba de Medianas")
+        print("3. Prueba de sort5.")
+        print("4. Timing Medianas.")
+        print("5. Timing sort5.")
+        print("6. Exportar CSV.")
+        print("\n7. Salir.")
         pass
